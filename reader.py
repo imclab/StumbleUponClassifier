f723e10459ae4205dec2da25311cf101c0f7763c
ghackeling <gavin.hackeling@vibrantmedia.com>
2013-09-12 15:31:33 -0400

    first commit

---
+++ reader.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+
+"""
+import numpy as np
+
+
+def main():
+    topicVectors = np.genfromtxt("/media/Storage/workspace/topicmodeling/src/main/resources/report.csv", delimiter=",")
+
+
+if __name__ == '__main__':
+    main()
